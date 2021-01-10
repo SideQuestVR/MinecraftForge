990619ce446971260df32f53b9ed7be64ab53391
Shane Harris <shane@sidequestvr.com>
2021-01-10 17:13:16 +0000

    latest

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-mkdir /var/lib/mc
-mkdir /var/lib/mc/minecraft
-mkdir /var/lib/mc/override
-mkdir /opt/minecraft-backups
-chmod 777 /var/lib/mc -R
+#!/bin/bash
+apt-get install docker.io docker-compose gnupg2 pass -y
+wget https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-secretservice-v0.6.3-amd64.tar.gz
+tar -xf docker-credential-secretservice-v0.6.3-amd64.tar.gz
+chmod +x docker-credential-secretservice
+mv docker-credential-secretservice /usr/bin/
+rm docker-credential-secretservice-v0.6.3-amd64.tar.gz
+mkdir /opt/minecraft /opt/minecraft/plugins /opt/minecraft/worlds /opt/minecraft/mods /opt/minecraft/config
+chmod 777 /opt/minecraft -R
+docker build -t sidequest/minecraft -f Dockerfile .
+docker swarm init
 docker stack deploy -c compose.yml sq_mc
